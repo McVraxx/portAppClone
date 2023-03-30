6fc528b4a3606d663121db3f64833b635affcd80
Karol Czoch <karol40777@gmail.com>
2023-03-30 21:35:36 +0200

    first commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="py-16 lg:section" id="contact">
+      <div className="container mx-auto">
+        <div className=" flex flex-col lg:flex-row">
+          <motion.div
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 flex  justify-start items-center"
+          >
+            <div>
+              <h4 className="text-x1 uppercase text-accent font-medium mb-2 tracking-wide">
+                Get in touch
+              </h4>
+              <h2 className="text-[45px] lg:text-[90px] leading-none mb-12">
+                Lets'work <br /> together!
+              </h2>
+            </div>
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6"
+          >
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              type="text"
+              placeholder="Your email"
+            />
+            <input
+              className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all"
+              type="text"
+              placeholder="Your name"
+            />
+            <textarea className="bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12"></textarea>
+            <button className="btn btn-lg"> Send message</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
